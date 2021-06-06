d640a9a4d42433d3cffb6b397a2d00c63538ca6f
Jenfrey Gudger <jenfrey.gudger@maymoility.com>
2021-06-06 16:02:38 -0400

    sol-4

--- solutions/mod-4-sol/script.js
+++ solutions/mod-4-sol/script.js
@@ -29,6 +29,74 @@ WARNING!!! WARNING!!!
 
 */
 
+
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// See Lecture 52, part 2
+
+var byeSpeaker = {};
+
+(function(name){
+
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+
+
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+ var speakWord = "Good Bye ";
+
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+ byeSpeaker.speak = function (name) {
+  console.log(speakWord + name);
+ }
+
+name.byeSpeaker = byeSpeaker;
+
+})(name);
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
+
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+var helloSpeaker = {};
+
+
+(function(name){
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+
+	// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+	var speakWord = "Hello ";
+
+	// STEP 4: Rewrite the 'speak' function such that it is attached to the
+	// helloSpeaker object instead of being a standalone function.
+	// See Lecture 52, part 2
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + name);
+	}
+
+	name.helloSpeaker = helloSpeaker;
+
+})(name);
+
+
+
+// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+// 'helloSpeaker' on the global scope as well.
+// See Lecture 52, part 2
+// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+// xxxx.xxxx = helloSpeaker;
+
+
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
@@ -41,27 +109,29 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+	for (var i = 0; i < names.length; i++) {
 
-  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt(0).toLowerCase();
+		
 
-  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
+	  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+	  // Retrieve the first letter of the current name in the loop.
+	  // Use the string object's 'charAt' function. Since we are looking for
+	  // names that start with either upper case or lower case 'J'/'j', call
+	  // string object's 'toLowerCase' method on the result so we can compare
+	  // to lower case character 'j' afterwards.
+	  // Look up these methods on Mozilla Developer Network web site if needed.
+	  var firstLetter = names[i].charAt(0).toLowerCase();
+	  console.log(firstLetter);
+	  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+	  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+	  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+	  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+	  // name in the loop.
+	  if (firstLetter === 'j') {
+	    byeSpeaker.speak(names[i]);
+	  } else {
+	    helloSpeaker.speak(names[i]);
+	  }
+	}
 
 })();
